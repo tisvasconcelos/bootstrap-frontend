fb0e123705568796e93c825804dcd5d104930148
Tiago Vasconcelos <tisvasconcelos@gmail.com>
2012-10-08 13:34:34 -0300

    Alteração no style e refatoração no js

--- scripts/helpers/utils.js
+++ scripts/helpers/utils.js
@@ -1,19 +1,30 @@
-//Método para pegar Object (JSON) e fazer um merge com outro Object
-Object.prototype.merge = function(object){    
-    var value;
-    for(key in object){
-        if(key!='merge'){
-            if(typeof object[key] == 'string')
-                value = object[key].toString().split(" ").join("");
-            else
-                value = object[key];
-            if(value != "" && value != null && value != undefined)
-                this[key] = value;
-        }
-    }    
-}
+var Utils = (function(){
+    _prototype = Utils.prototype;
 
-//Método que faz multiplos replace
-String.prototype.replaceAll = function(find, replace) {
-    return this.split(find).join(replace);
-}
+    //Constructor
+    function Utils(){
+        var _self = this;
+    }
+    
+    //Método para pegar Object (JSON) e fazer um merge com outro Object
+    _prototype.merge = function(object, merge){
+        var value;
+        for(key in object){
+            if(key!='merge'){
+                if(typeof object[key] == 'string')
+                    value = object[key].toString().split(" ").join("");
+                else
+                    value = object[key];
+                if(value != "" && value != null && value != undefined)
+                    merge[key] = value;
+            }
+        }    
+    };
+
+    //Método que faz multiplos replace
+    _prototype.replaceAll = function(value, find, replace) {
+        return value.split(find).join(replace);
+    };
+
+    return Utils;
+})();
---
+++ scripts/tests/tests.config.js
@@ -0,0 +1,5 @@
+require.config({baseUrl: "http://tisvasconcelos.github.com/bootstrap-frontend/scripts/",waitSeconds: 15});
+
+QUnit.done(function(){
+	$('#workspace').remove();
+});
